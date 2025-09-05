b1a50720cf7435776d0e018ade9f594b43d663c1
cityofsmiles <cityofsmiles@gmail.com>
2025-09-05 03:53:14 +0000

    edited github

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // ✅ Replace with your GitHub repo name
-const repoName = "https://github.com/cityofsmiles/basic-algebra-flashcards.git";
+const repoName = "cityofsmiles/basic-algebra-flashcards";
 
 export default defineConfig({
   plugins: [react()],
